c8f5e14fd8053b60523722b47badabbebff51368
sanjai06 <nsksanjai@gmail.com>
2023-12-30 19:39:46 +0530

    new update

--- src/pages/eventsmanagement/AddEvents.jsx
+++ src/pages/eventsmanagement/AddEvents.jsx
@@ -101,7 +101,7 @@ export default function Createcampaign() {
                             type="submit"
                             className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm "
                         >
-                            Create Campaign
+                            Create Events
                         </button>
                     </div>
                 </form>
